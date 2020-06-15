e107cadc79fe072ac615ef24f950d87284121458
Ricardo <rokoneitor@gmail.com>
2020-06-14 22:59:15 -0500

    cliente v 1.0

--- cliente_calculadora.py
+++ cliente_calculadora.py
@@ -3,6 +3,35 @@ PUERTO = 8000
 
 server = ServerProxy('http://localhost:' + str(PUERTO), allow_none=True)
 
-print(server.prefijo('Expresion en prefijo xd'))
-print(server.infijo('Expresion en infijo xd'))
-print(server.posfijo('Expresion en posfijo xd'))
+while(True):
+    print("¿Qué operación deseas realizar?")
+    print("1.- Evaluar una expresión en notación prefija")
+    print("2.- Evaluar una expresión en notación infija")
+    print("3.- Evaluar una expresión en notación posfija")
+    print("4.- Salir")
+
+    respuesta = int(input())
+
+    # Evaluar expresión prefija
+    if(respuesta == 1):
+        print("\nNel, todavía no xD\n")
+
+    elif(respuesta == 2):
+        print("\nIngresa tu expresión: ")
+        expresion = input()
+
+        print("\nResultado de evaluar la expresión: ")
+        print(str(server.infijo(expresion)) + '\n')
+
+    elif(respuesta == 3):
+        print("\nIngresa tu expresión: ")
+        expresion = input()
+
+        print("\nResultado de evaluar la expresión: ")
+        print(str(server.posfijo(expresion)) + '\n')
+
+    elif(respuesta == 4):
+        break
+
+    else:
+        print("\nIngresa una opción válida\n")
